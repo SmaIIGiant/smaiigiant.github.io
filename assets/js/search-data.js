3be360c477f0f8fb832fd2348c176a9f249d858b
SmaIIGiant <122044780+SmaIIGiant@users.noreply.github.com>
2025-10-26 00:59:49 +0000

    Deploying to gh-pages from @ SmaIIGiant/smaiigiant.github.io@2e0fb67ba217c36291a45c31645cf1cad5e7458d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-yihao-geng",
+    title: "Yihao Geng",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
